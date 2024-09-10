fe2a1945f2ed6c254f19c4b6450b43fdca285c65
PBGaming11 <phanbaobeo789@gmail.com>
2024-09-10 10:20:34 +0700

    updateuser

--- Areas/Admin/Controllers/UsersController.cs
+++ Areas/Admin/Controllers/UsersController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Identity;
 using WebsiteTMDT.Models;
 using System.Threading.Tasks;
 using WebsiteTMDT.Areas.Admin.Models.EF;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace WebsiteTMDT.Areas.Admin.Controllers
 {
@@ -34,14 +36,26 @@ namespace WebsiteTMDT.Areas.Admin.Controllers
                 return NotFound();
             }
 
-            var userRoles = await _userManager.GetRolesAsync(user);
-            var allRoles = _roleManager.Roles.ToList();
+            // Lấy tất cả các role từ database
+            var roles = await _roleManager.Roles.ToListAsync();
 
+            // Chuyển đổi từ IdentityRole thành SelectListItem
+            var allRoles = roles.Select(role => new SelectListItem
+            {
+                Value = role.Name,
+                Text = role.Name
+            }).ToList();
+
+            // Lấy danh sách role hiện tại của user
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            // Tạo model và truyền dữ liệu cho view
             var model = new EditUserViewModel
             {
                 User = user,
                 Roles = userRoles,
-                AllRoles = allRoles
+                AllRoles = allRoles,  // Gán danh sách SelectListItem
+                SelectedRoles = userRoles.ToArray()  // Nếu bạn muốn hiển thị role đã được chọn
             };
 
             return View(model);
@@ -52,23 +66,43 @@ namespace WebsiteTMDT.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(EditUserViewModel model)
         {
+            if (ModelState.IsValid)
+            {
+                return View(model);
+            }
+
             var user = await _userManager.FindByIdAsync(model.User.Id);
             if (user == null)
             {
                 return NotFound();
             }
 
+            // Cập nhật thông tin người dùng (không yêu cầu mật khẩu)
             user.Email = model.User.Email;
             user.UserName = model.User.UserName;
 
             var result = await _userManager.UpdateAsync(user);
-
             if (!result.Succeeded)
             {
                 ModelState.AddModelError("", "Cannot update user");
                 return View(model);
             }
 
+            // Kiểm tra nếu có mật khẩu mới được nhập
+            if (!string.IsNullOrEmpty(model.NewPassword))
+            {
+                // Cập nhật mật khẩu mới cho user nếu có
+                var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+                var passwordChangeResult = await _userManager.ResetPasswordAsync(user, token, model.NewPassword);
+
+                if (!passwordChangeResult.Succeeded)
+                {
+                    ModelState.AddModelError("", "Cannot change password");
+                    return View(model);
+                }
+            }
+
+            // Cập nhật role cho người dùng
             var userRoles = await _userManager.GetRolesAsync(user);
             var selectedRoles = model.SelectedRoles ?? new string[] { };
 
@@ -78,10 +112,7 @@ namespace WebsiteTMDT.Areas.Admin.Controllers
             await _userManager.RemoveFromRolesAsync(user, rolesToRemove);
             await _userManager.AddToRolesAsync(user, rolesToAdd);
 
-            // Redirect về action Index trong controller Users trong area Admin 
             return RedirectToAction("Index", "Users", new { area = "Admin" });
-
         }
-
     }
 }
--- Areas/Admin/Models/EF/EditUserViewModel.cs
+++ Areas/Admin/Models/EF/EditUserViewModel.cs
@@ -1,12 +1,18 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 
 namespace WebsiteTMDT.Areas.Admin.Models.EF
 {
+    // EditUserViewModel.cs
     public class EditUserViewModel
     {
         public IdentityUser User { get; set; }
         public IList<string> Roles { get; set; }
-        public IList<IdentityRole> AllRoles { get; set; }
+        public IList<SelectListItem> AllRoles { get; set; }  // Sử dụng SelectListItem cho dropdown
         public string[] SelectedRoles { get; set; }
+        public string CurrentRole { get; set; }  // Trường để lưu role hiện tại
+        public string NewPassword { get; set; }  // Trường để cập nhật mật khẩu mới
     }
+
 }
